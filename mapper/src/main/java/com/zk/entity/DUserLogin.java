60e6ad7a64dda84f0f165a90a7901b3d2c93771c
zengkai <453055141@qq.com>
2018-08-22 22:30:14 +0800

    mybatisplus整合

--- mapper/src/main/java/com/zk/entity/DUserLogin.java
+++ mapper/src/main/java/com/zk/entity/DUserLogin.java
@@ -2,9 +2,11 @@ package com.zk.entity;
 
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
-import java.time.LocalDateTime;
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.extension.activerecord.Model;
+
 import java.io.Serializable;
+import java.time.LocalDateTime;
 
 /**
  * <p>
--- mapper/src/main/java/com/zk/entity/Members.java
+++ mapper/src/main/java/com/zk/entity/Members.java
@@ -1,10 +1,12 @@
 package com.zk.entity;
 
 import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableId;
-import java.time.LocalDateTime;
 import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.extension.activerecord.Model;
+
 import java.io.Serializable;
+import java.time.LocalDateTime;
 
 /**
  * <p>
