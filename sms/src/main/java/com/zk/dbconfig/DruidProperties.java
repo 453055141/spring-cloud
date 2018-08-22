a12867bc755d62e49655b9225111c5b4540aab3c
zengkai <453055141@qq.com>
2018-08-22 20:57:18 +0800

    多数据源分布式事务配置

---
+++ sms/src/main/java/com/zk/dbconfig/DruidProperties.java
@@ -0,0 +1,22 @@
+package com.zk.dbconfig;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * @Description
+ * @Author zengkai
+ * @Date 2018/8/22 14:01
+ */
+@Component
+@ConfigurationProperties(prefix = "spring.datasource.dynamic.master")
+public class DruidProperties {
+
+    private String url = "url";
+
+    private String username = "root";
+
+    private String password = "admin";
+
+    private String driverClassName = "com.mysql.jdbc.Driver";
+}
